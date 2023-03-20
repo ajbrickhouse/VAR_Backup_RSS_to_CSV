922cf65f0e243a4a96cede271c8e435df08eb83a
ajbrickhouse <106787118+ajbrickhouse@users.noreply.github.com>
2023-03-19 23:45:56 -0500

    Refactored with ChatGPT

--- Sysmac_Var_Backup_RSS_to_CSV.py
+++ Sysmac_Var_Backup_RSS_to_CSV.py
@@ -1,78 +1,62 @@
 # Created by Anthony Brinkhuis
 import struct
 import xml.etree.ElementTree as et
-import pandas as pd 
+import pandas as pd
 
 
-# The filename with NO EXTENSION
-file_name = r'examples\controller_1_ALL_DATA_041622'
+class SysmacData:
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-# Convert Sysmac hex values to real number
-def hex_to_real(hex_val):
-    # Convert the hex val to binary
-    binary_data = bytes.fromhex(hex_val)
-    # define the format code: 'e' (16bit), 'f' (32 bit), and 'd' (64 bit)
-    FLOAT = 'f'
-    # Create format string
-    fmt = '<' + FLOAT * (len(binary_data) // struct.calcsize(FLOAT))
-    # Unpack from the buffer
-    numbers = struct.unpack(fmt, binary_data)
-    # Return the real number
-    return numbers[0]
+    @staticmethod
+    def hex_to_real(hex_val):
+        binary_data = bytes.fromhex(hex_val)
+        FLOAT = 'f'
+        fmt = '<' + FLOAT * (len(binary_data) // struct.calcsize(FLOAT))
+        numbers = struct.unpack(fmt, binary_data)
+        return numbers[0]
 
-# Parse the XML file
-xml_data = et.parse(f'{file_name}.xml')
+    def parse_xml(self):
+        xml_data = et.parse(f'{self.file_name}.xml')
+        data_root = xml_data.find('Body')
+        ret_vars = data_root.find('RetainVariable')
 
-# Select the body tree
-data_root = xml_data.find('Body')
+        lst_tag = []
+        lst_data = []
+        lst_type = []
 
-# Select RetainVariable branch
-ret_vars = data_root.find('RetainVariable')
+        for item in ret_vars:
+            tag = item.items()[0][1][6:]
+            data_type = item.items()[1][1]
+            data = item.find('Data')
+            data_text = data.text
 
-# Defined list for items that will be save to CSV
-lst_tag = []
-lst_data = []
-lst_type = []
+            if ('REAL' in data_type) or ('real' in data_type):
+                lst_tag.append(tag)
+                lst_data.append(self.hex_to_real(data_text))
+                lst_type.append(data_type)
+            elif ('STRING' in data_type) or ('string' in data_type):
+                lst_tag.append(tag)
+                if data_text is not None:
+                    lst_data.append(bytearray.fromhex(data_text).decode())
+                else:
+                    lst_data.append('')
+                lst_type.append(data_type)
+            else:
+                lst_tag.append(tag)
+                lst_data.append(data.text)
+                lst_type.append(data_type)
 
-# Cycle through all items in the RetainVariable branch
-for item in ret_vars:
-    # Get data from items
-    tag = item.items()[0][1][6:]
-    data_type = item.items()[1][1]
-    # get the data sub branch
-    data = item.find('Data')
-    data_text = data.text
-    # if the data type is real
-    if ('REAL' in data_type) or ('real' in data_type):
-        # append info into our lists
-        lst_tag.append(tag)
-        # convert the hex value to something readable
-        lst_data.append(hex_to_real(data_text))
-        lst_type.append(data_type)
-    # if the data type is a string
-    elif ('STRING' in data_type) or ('string' in data_type):
-        if data_text != None:
-            lst_tag.append(tag)
-            # Convert the hex value to something readable
-            lst_data.append(bytearray.fromhex(data_text).decode())
-            lst_type.append(data_type)
-        else:
-            lst_tag.append(tag)
-            # Convert the hex value to something readable
-            lst_data.append('')
-            lst_type.append(data_type)
-    # if nothing needs to be decoded then this
-    else:
-        lst_tag.append(tag)
-        lst_data.append(data.text)
-        lst_type.append(data_type)
+        data_dict = {'Tag': lst_tag, 'Data': lst_data, 'Type': lst_type}
+        return data_dict
 
-# Place all of the accumulated list data into a dictionary
-# This makes it more easy to save as a CSV
-data_dict = {'Tag': lst_tag, 'Data': lst_data, 'Type': lst_type}  
+    def save_to_csv(self):
+        data_dict = self.parse_xml()
+        df = pd.DataFrame(data_dict)
+        df.to_csv(f'{self.file_name}.csv')
 
-# Convert the dictionary to a dataframe
-df = pd.DataFrame(data_dict)
 
-# Save the dataframe as a CSV
-df.to_csv(f'{file_name}.csv')
+if __name__ == '__main__':
+    file_name = r'examples\controller_1_ALL_DATA_041622'
+    sysmac_data = SysmacData(file_name)
+    sysmac_data.save_to_csv()
